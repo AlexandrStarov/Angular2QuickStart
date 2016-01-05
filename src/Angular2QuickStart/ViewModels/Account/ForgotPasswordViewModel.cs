daa6c8fd166449cb440d762ca7a9f128aec1b982
Alexandr Starov <alexandr.starovv@gmail.com>
2016-01-05 21:20:06 +0200

    Empty project.

---
+++ src/Angular2QuickStart/ViewModels/Account/ForgotPasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular2QuickStart.ViewModels.Account
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
